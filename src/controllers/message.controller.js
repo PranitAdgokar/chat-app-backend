b0fcbcd727814996dc8d4ddda41fecd2b264a3be
PranitAdgokar <136226664+PranitAdgokar@users.noreply.github.com>
2024-12-25 02:18:44 +0530

    implement messaging feature with message model, controller, and routes

---
+++ src/controllers/message.controller.js
@@ -0,0 +1,58 @@
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filterUsersId = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+    res.status(200).json(filterUsersId);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+    const messages = await Message.find({
+      $or: [
+        { sender: myId, receiver: userToChatId },
+        { sender: userToChatId, receiver: myId },
+      ],
+    });
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudnary.uploader.upload("image");
+      imageUrl = uploadResponse.url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+    await newMessage.save();
+    //realtime message sending functionallty will be added here
+    req.status(200).json(newMessage);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
+};
