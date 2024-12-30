d97e954ae5c793f7ba5820ee944e952b5aaeb4ab
PranitAdgokar <136226664+PranitAdgokar@users.noreply.github.com>
2024-12-30 10:51:25 +0530

    fix: update CORS origin to allow requests from the correct frontend URL

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5176", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
