e4ecf4da22da0da9a8417c9c020f6865306437fb
PranitAdgokar <136226664+PranitAdgokar@users.noreply.github.com>
2025-01-30 15:31:54 +0530

    feat: update CORS configuration to allow requests from the production frontend

--- src/index.js
+++ src/index.js
@@ -11,11 +11,19 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    //  origin: "http://localhost:5173",
+    origin: "https://chat-app-fronend-seven.vercel.app/", // Allow your frontend
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 server.listen(PORT, () => {
   console.log("Server is running on port:" + PORT);
+
   connectDB();
 });
